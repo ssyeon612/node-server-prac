8d1d3fb63d008c67953d735c3a75eb14fbf4bcfb
ssyeon612 <ssyeon612@gmail.com>
2020-09-16 11:52:17 +0900

    Update: add mysql

--- app.js
+++ app.js
@@ -1,6 +1,17 @@
 var express = require('express')
 var app = express()
 var bodyParser = require('body-parser')
+var mysql = require('mysql')
+
+var connection = mysql.createConnection({
+    host: 'localhost',
+    port: 3306,
+    user: 'appg',
+    password: 'appg',
+    database: 'jsnode'
+})
+
+connection.connect();
 
 app.listen(3000, function () {
     console.log("start!!!! express server on port 3000");
@@ -8,26 +19,37 @@ app.listen(3000, function () {
 
 app.use(express.static('public'))
 app.use(bodyParser.json())
-app.use(bodyParser.urlencoded({extended:true}))
+app.use(bodyParser.urlencoded({extended: true}))
 app.set('view engine', 'ejs')
 
-app.get('/', function(req, res){
+app.get('/', function (req, res) {
     //res.send("<h1>hi friend</h1>")
     res.sendFile(__dirname + "/public/main.html")
 });
 
-app.get('/main', function(req, res){
+app.get('/main', function (req, res) {
     res.sendFile(__dirname + "/public/main.html")
 });
 
-app.post('/email_post', function(req, res) {
+app.post('/email_post', function (req, res) {
     console.log(req.body.email)
     //res.send("<h1>welcome! " + req.body.email +"</h1>")
-    res.render('email.ejs', {'email' : req.body.email})
+    res.render('email.ejs', {'email': req.body.email})
 });
 
-app.post('/ajax_send_mail', function(req, res){
-    console.log(req.body.email);
-    var responseData = {'result': 'ok', 'email' : req.body.email}
-    res.json(responseData);
+app.post('/ajax_send_mail', function (req, res) {
+    var email = req.body.email;
+    var responseData = {};
+
+    var query = connection.query('select name from user where email="' + email + '"', function (err, rows) {
+        if (err) throw err;
+        if (rows[0]) {
+            responseData.result = "ok";
+            responseData.name = rows[0].name;
+        } else {
+            responseData.result = "none";
+            responseData.name = "";
+        }
+        res.json(responseData);
+    });
 })
